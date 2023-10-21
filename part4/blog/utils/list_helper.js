9acd9b187e3b824cb9ad58786da679b92a6a1e66
rhexa <akun.kripitit@gmail.com>
2023-10-21 20:31:24 +0300

    part 4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,7 +6,15 @@ const totalLikes = (blogs) => {
   return blogs.reduce((n, {likes}) => n + likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const {title, author, likes} = blogs.reduce((maxLikesBlog, currentBlog) => {
+    return currentBlog.likes > maxLikesBlog.likes ? currentBlog : maxLikesBlog
+  }, blogs[0])
+  return {title, author, likes}
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
