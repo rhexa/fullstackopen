296181885b578e7db98a219d8a9e994424f5e1be
rhexa <akun.kripitit@gmail.com>
2024-10-10 14:12:28 +0300

    part 5.1

---
+++ part5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,91 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import Notification from './components/Notification'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs(blogs)
+    )
+  }, [])
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>log in to application</h2>
+
+        {errorMessage &&
+          <Notification type="error" timeout={5} hook={setErrorMessage} message={errorMessage} />
+        }
+
+        {loginForm()}
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+
+      {errorMessage &&
+        <Notification type="error" timeout={5} hook={setErrorMessage} message={errorMessage} />
+      }
+
+      <p>{user.name} logged in</p>
+
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+
+    </div>
+  )
+}
+
+export default App
