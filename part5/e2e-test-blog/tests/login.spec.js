9801e5418ac6baa31b24169708a773271ab44827
rhexa <akun.kripitit@gmail.com>
2024-10-29 12:30:37 +0200

    part 5.20

--- part5/e2e-test-blog/tests/login.spec.js
+++ part5/e2e-test-blog/tests/login.spec.js
@@ -59,31 +59,52 @@ describe('Blog app', () => {
 
       await expect(lfailedLogin).toBeVisible()
     })
+  })
 
-    describe('When logged in', () => {
-      beforeEach(async ({ page }) => {
-        const lusername = await page.locator('input[name="Username"]')
-        const lpassword = await page.locator('input[name="Password"]')
-        const lloginButton = await page.getByRole('button', { name: 'login' })
-        
-        await lusername.fill('testuser')
-        await lpassword.fill('testpassword')
-        await lloginButton.click()
-      })
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+      const lusername = await page.locator('input[name="Username"]')
+      const lpassword = await page.locator('input[name="Password"]')
+      const lloginButton = await page.getByRole('button', { name: 'login' })
+
+      await lusername.fill('testuser')
+      await lpassword.fill('testpassword')
+      await lloginButton.click()
+    })
+
+    const createTestBlog = async ({ page }) => {
+      await page.getByRole('button', { name: 'new blog' }).click()
+
+      await page.locator('input[name="Title"]').fill('Test Blog')
+      await page.locator('input[name="Author"]').fill('Test Author')
+      await page.locator('input[name="Url"]').fill('https://example.com')
+      await page.getByRole('button', { name: 'create' }).click()
+    }
+
+    // Create a test that verifies that a logged in user can create a blog
+    test('a new blog can be created', async ({ page }) => {
+      await createTestBlog({ page })
+
+      await expect(await page.locator('h2[class="blog-title"]').getByText('Test Blog')).toBeVisible()
+      await expect(await page.locator('p[class="blog-author"]').getByText('Test Author')).toBeVisible()
+      await expect(await page.getByText('https://example.com')).toBeHidden()
+    })
+
+    // Do a test that makes sure the blog can be liked
+    test('a blog can be liked', async ({ page }) => {
+      await createTestBlog({ page })
+
+      await page.getByRole('button', { name: 'view' }).click()
+
+      await expect(await page.locator('.togglableContent').first().getByText('likes')).toBeVisible()
+
+      const currentLikes = await (await page.locator('.togglableContent').first().getByText('likes 0').innerHTML()).match(/\d+/)[0]
       
-      // Create a test that verifies that a logged in user can create a blog
-      test('a new blog can be created', async ({ page }) => {
-        await page.getByRole('button', { name: 'new blog' }).click()
+      await page.getByRole('button', { name: 'like' }).first().click()
 
-        await page.locator('input[name="Title"]').fill('Test Blog')
-        await page.locator('input[name="Author"]').fill('Test Author')
-        await page.locator('input[name="Url"]').fill('https://example.com')
-        await page.getByRole('button', { name: 'create' }).click()
+      const newLikes = await (await page.locator('.togglableContent').first().getByText('likes 1').innerHTML()).match(/\d+/)[0]
 
-        await expect(await page.locator('h2[class="blog-title"]').getByText('Test Blog')).toBeVisible()
-        await expect(await page.locator('p[class="blog-author"]').getByText('Test Author')).toBeVisible()
-        await expect(await page.getByText('https://example.com')).toBeHidden()
-      })
+      expect(parseInt(newLikes)).toBe(parseInt(currentLikes) + 1)
     })
   })
 })
