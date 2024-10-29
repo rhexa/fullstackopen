11b8315f2593d13120523fac118bb5db58a87d52
rhexa <akun.kripitit@gmail.com>
2024-10-29 20:09:17 +0200

    part 5.23

--- part5/e2e-test-blog/tests/login.spec.js
+++ part5/e2e-test-blog/tests/login.spec.js
@@ -2,7 +2,13 @@
 // Make a test to ensure that the application displays the login form by default.
 
 const { test, expect, beforeEach, describe } = require('@playwright/test')
-const { log } = require('console')
+const { faker } = require('@faker-js/faker')
+
+const randomBlog = () => ({
+  title: faker.lorem.words(3),
+  author: faker.person.fullName(),
+  url: faker.internet.url(),
+})
 
 const testBlog = {
   title: 'Test Blog',
@@ -23,12 +29,22 @@ const createBlog = async (page, blog) => {
   await page.locator('input[name="Author"]').fill(blog.author)
   await page.locator('input[name="Url"]').fill(blog.url)
   await page.getByRole('button', { name: 'create' }).click()
+
+  await page.getByRole('heading', { name: blog.title }).waitFor()
 }
 
 const loginWith = async (page, username, password) => {
   await page.locator('input[name="Username"]').fill(username)
   await page.locator('input[name="Password"]').fill(password)
   await page.getByRole('button', { name: 'login' }).click()
+}
+
+// TODO: create a function to check if an array is sorted by the greatest number first
+const isSorted = (obj) => {
+  const before = [...obj]
+  let after = [...obj]
+  after = after.sort((a, b) => b - a)
+  return JSON.stringify(before) === JSON.stringify(after)
 }
 
 describe('Blog app', () => {
@@ -142,6 +158,70 @@ describe('Blog app', () => {
       await page.locator('h2[class="blog-title"]').getByText(testBlog.title).waitFor()
       const lremoveButton = await page.getByRole('button', { name: 'remove' }).waitFor({ state: 'detached' })
       await expect(lremoveButton).toBeFalsy
+    })
+
+    // Do a test that ensures that the blogs are arranged in the order according to the likes, the blog with the most likes first.
+    test('blogs are arranged in the order according to the likes count', async ({ page }) => {
+      // create 3 blogs
+      const initBlogs = Array(3).fill().map(() => randomBlog())
+      await createBlog(page, initBlogs[0])
+      await createBlog(page, initBlogs[1])
+      await createBlog(page, initBlogs[2])
+
+      const lblogs = await page.locator(':has(> h2.blog-title)')
+
+      // verify that the 3 blogs are created
+      await expect(await lblogs.count()).toEqual(initBlogs.length)
+
+      // 2nd blog is liked 5 times
+      await lblogs.nth(1).getByRole('button', { name: 'view' }).click()
+      let ldetails = await lblogs.nth(1).locator('xpath=..')
+      await ldetails.getByRole('button', { name: 'like' }).waitFor({ state: 'visible' })
+      for (let i = 0; i < 5; i++) {
+        let currentLikes = parseInt(await (await ldetails.getByText('likes').innerHTML()).match(/\d+/)[0])
+        await ldetails.getByRole('button', { name: 'like' }).click()
+        await ldetails.getByText('likes ' + (currentLikes + 1)).waitFor()
+      }
+      await expect(ldetails.getByText('likes 5')).toBeVisible()
+
+      // 3rd blog is liked 3 times
+      await lblogs.nth(2).getByRole('button', { name: 'view' }).click()
+      ldetails = await lblogs.nth(2).locator('xpath=..')
+      await ldetails.getByRole('button', { name: 'like' }).waitFor({ state: 'visible' })
+      for (let i = 0; i < 3; i++) {
+        let currentLikes = parseInt(await (await ldetails.getByText('likes').innerHTML()).match(/\d+/)[0])
+        await ldetails.getByRole('button', { name: 'like' }).click()
+        await ldetails.getByText('likes ' + (currentLikes + 1)).waitFor()
+      }
+      await expect(ldetails.getByText('likes 3')).toBeVisible()
+
+      // get all blogs likes
+      const llikes = await lblogs.locator('xpath=..').getByText('likes').all()
+      let likes = []
+      for (const llike of llikes) {
+        const llikeHtml = await llike.innerHTML()
+        const extractedlike = parseInt(llikeHtml.match(/\d+/)[0])
+        likes.push(extractedlike)
+      }
+      
+      // likes should be unsorted
+      expect(isSorted(likes)).toBeFalsy()
+
+      // sort likes
+      await page.locator('select[name="sort-blogs"]').selectOption('likes')
+      await page.locator('select[name="sort-blogs"]').waitFor()
+
+      // get all blogs likes
+      const llikesAfterSort = await lblogs.locator('xpath=..').getByText('likes').all()
+      let likesAfterSort = []
+      for (const llike of llikesAfterSort) {
+        const llikeHtml = await llike.innerHTML()
+        const extractedlike = parseInt(llikeHtml.match(/\d+/)[0])
+        likesAfterSort.push(extractedlike)
+      }
+
+      // likes should be sorted
+      expect(isSorted(likesAfterSort)).toBeTruthy()
     })
   })
 })
