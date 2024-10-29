229f3408d67102a784d3524316de8385361b2ce1
rhexa <akun.kripitit@gmail.com>
2024-10-29 11:40:16 +0200

    part 5.19

--- part5/e2e-test-blog/tests/login.spec.js
+++ part5/e2e-test-blog/tests/login.spec.js
@@ -45,19 +45,45 @@ describe('Blog app', () => {
 
       await expect(await page.getByText(`Test User logged in`)).toBeVisible()
     })
-    
+
     test('fails with wrong credentials', async ({ page }) => {
       const lusername = await page.locator('input[name="Username"]')
       const lpassword = await page.locator('input[name="Password"]')
       const lloginButton = await page.getByRole('button', { name: 'login' })
-  
+
       await lusername.fill('wronguser')
       await lpassword.fill('wrongpassword')
       await lloginButton.click()
 
-      const lfailedLogin =await page.locator('.notification.error').getByText('Wrong credentials')
+      const lfailedLogin = await page.locator('.notification.error').getByText('Wrong credentials')
 
       await expect(lfailedLogin).toBeVisible()
+    })
+
+    describe('When logged in', () => {
+      beforeEach(async ({ page }) => {
+        const lusername = await page.locator('input[name="Username"]')
+        const lpassword = await page.locator('input[name="Password"]')
+        const lloginButton = await page.getByRole('button', { name: 'login' })
+        
+        await lusername.fill('testuser')
+        await lpassword.fill('testpassword')
+        await lloginButton.click()
+      })
+      
+      // Create a test that verifies that a logged in user can create a blog
+      test('a new blog can be created', async ({ page }) => {
+        await page.getByRole('button', { name: 'new blog' }).click()
+
+        await page.locator('input[name="Title"]').fill('Test Blog')
+        await page.locator('input[name="Author"]').fill('Test Author')
+        await page.locator('input[name="Url"]').fill('https://example.com')
+        await page.getByRole('button', { name: 'create' }).click()
+
+        await expect(await page.locator('h2[class="blog-title"]').getByText('Test Blog')).toBeVisible()
+        await expect(await page.locator('p[class="blog-author"]').getByText('Test Author')).toBeVisible()
+        await expect(await page.getByText('https://example.com')).toBeHidden()
+      })
     })
   })
 })
