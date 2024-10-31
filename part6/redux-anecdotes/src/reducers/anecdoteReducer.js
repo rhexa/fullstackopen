90d717df7d42635f75f9576bfd8461efa1d41a9d
rhexa <akun.kripitit@gmail.com>
2024-10-31 21:46:09 +0200

    part 6.4

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -33,6 +33,9 @@ const reducer = (state = initialState, action) => {
       }
       return state.map(a => a.id === id ? voted : a)
     }
+    case 'NEW_ANECDOTE': {
+      return [...state, asObject(action.data.content)]
+    }
     default: return state
   }
 }
