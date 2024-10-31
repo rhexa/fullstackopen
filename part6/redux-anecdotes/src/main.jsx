9818c8ab7d34ec5d6b021def572f762a2fc7caaa
rhexa <akun.kripitit@gmail.com>
2024-10-31 21:33:46 +0200

    part 6.3

---
+++ part6/redux-anecdotes/src/main.jsx
@@ -0,0 +1,20 @@
+import ReactDOM from 'react-dom/client'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './App'
+import reducer from './reducers/anecdoteReducer'
+
+const store = createStore(reducer)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
+}
+
+renderApp()
+store.subscribe(renderApp)
