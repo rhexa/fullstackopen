8ce3281e93d11e106846e130613303edb7717f8b
rhexa <akun.kripitit@gmail.com>
2024-11-02 15:47:47 +0200

    part 6.14

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,20 +1,28 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { vote } from '../reducers/anecdoteReducer'
+import { setAnecdote, vote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
+import { useEffect } from 'react'
 
 const AnecdoteList = () => {
+  const dispatch = useDispatch()
+
   const anecdotes = useSelector(({ anecdotes, filter }) => {
     const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes)
     return filter ? sortedAnecdotes.filter(a => a.content.toLowerCase().includes(filter.toLowerCase())) : sortedAnecdotes
   })
-
-  const dispatch = useDispatch()
 
   const handleVote = ({ id, content }) => {
     console.log('vote', id)
     dispatch(vote(id))
     dispatch(setNotification(`you voted '${content}'`))
   }
+
+  useEffect(() => {
+    anecdoteService.getAll()
+      .then(anecdotes => dispatch(setAnecdote(anecdotes)))
+  }, [])
+  
   return (
     <>
       {anecdotes.map(anecdote =>
