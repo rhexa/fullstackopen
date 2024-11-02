de6bf8fc213fadbbd1cfb23fb528e51b850bc96c
rhexa <akun.kripitit@gmail.com>
2024-11-02 16:22:56 +0200

    part 6.15

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,13 +1,20 @@
 import { useDispatch } from "react-redux"
-import { addAnecdote } from "../reducers/anecdoteReducer";
-import { setNotification } from "../reducers/notificationReducer";
+import { addAnecdote } from "../reducers/anecdoteReducer"
+import { setNotification } from "../reducers/notificationReducer"
+import anecdoteService from "../services/anecdotes"
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
-  
-  const handleAddAnecdote = (event) => {
+
+  const handleAddAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
+    try {
+      await anecdoteService.createNew(content)
+    } catch (error) {
+      console.log('here we got an error', error)
+      return
+    }
     dispatch(addAnecdote(content))
     dispatch(setNotification(`you added a new anecdote '${content}'`))
     event.target.anecdote.value = ''
